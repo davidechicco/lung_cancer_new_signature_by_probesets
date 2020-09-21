06310619a21af4835dc2890bf8d2380c529d87a7
davidechicco <you@example.com>
2020-09-21 12:47:22 -0400

    added scripts retrieve sequences

--- bin/probesToGeneAssociation_new_signature.r
+++ bin/probesToGeneAssociation_new_signature.r
@@ -32,16 +32,16 @@ gpl96_ann <- readGEOAnn(GEOAccNum = thisGEOplatform)
 gpl96_ann_df <- as.data.frame(gpl96_ann, stringsAsFactors=FALSE)
 
 
-probesList <- c("201856_s_at", "201911_s_at", "202688_at", "204511_at", "204999_s_at", "207730_x_at", "208471_at", "209985_s_at", "210065_s_at", "211013_x_at", "211838_x_at", "212976_at", "213439_x_at", "213770_at", "214730_s_at", "214734_at", "215066_at", "215604_x_at", "217989_at", "218164_at", "222155_s_at", "222168_at", "37152_at")
+MCM5_signature_probesets <- c("200785_s_at", "201856_s_at", "201926_s_at", "202085_at", "202170_s_at", "202922_at", "204511_at", "205364_at", "206472_s_at", "207730_x_at", "208664_s_at", "208892_s_at", "210404_x_at", "211741_x_at", "212110_at", "213778_x_at", "214261_s_at", "214730_s_at", "215066_at", "216735_x_at", "218380_at", "220389_at", "221610_s_at", "222086_s_at", "222168_at")
 
 #    patients_data_filtered_ourSignature <- (gene_expression_with_labels[gene_expression_with_labels$ID %in% ourSignature, ])
     
 cat("probeset IDs:\n")
-gpl96_ann_ID_list <- gpl96_ann_df[gpl96_ann_df$"ID" %in% probesList,]
+gpl96_ann_ID_list <- gpl96_ann_df[gpl96_ann_df$"ID" %in% MCM5_signature_probesets,]
 cat(gpl96_ann_ID_list$"Gene Symbol", sep="\n")
 cat("\n")
 
-print(gpl96_ann_ID_list[,c("ID", "Gene Symbol")], row.names = FALSE)
+print(gpl96_ann_ID_list[,c("ID", "Gene Symbol", "RefSeq Transcript ID")], row.names = FALSE)
 
 print(cat(gpl96_ann_ID_list[,c("Gene Symbol")], sep="\", \""))
 
